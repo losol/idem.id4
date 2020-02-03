254a5d131e488662d28094101079c356e3f6a079
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-03 23:24:55 +0300

    Code cleanup & Modifications copyright (c) 2020 Losol AS

--- src/Losol.Identity/Controllers/Device/DeviceController.cs
+++ src/Losol.Identity/Controllers/Device/DeviceController.cs
@@ -1,6 +1,6 @@
 // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
+// Modifications copyright (c) 2020 Losol AS
 
 using System;
 using System.Linq;
