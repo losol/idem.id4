84d85ec3d499f17082cd568f3d648cb979a29edf
Andrew Anisimov <anisimov.new@gmail.com>
2020-02-04 09:48:39 +0300

    Code cleanup.

--- demo/Losol.Identity.DemoAdminJSClient/Program.cs
+++ demo/Losol.Identity.DemoAdminJSClient/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Losol.Identity.DemoJavaScriptClient
+namespace Losol.Identity.DemoAdminJSClient
 {
     public class Program
     {
--- demo/Losol.Identity.DemoAdminJSClient/Startup.cs
+++ demo/Losol.Identity.DemoAdminJSClient/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Losol.Identity.DemoJavaScriptClient
+namespace Losol.Identity.DemoAdminJSClient
 {
     public class Startup
     {
